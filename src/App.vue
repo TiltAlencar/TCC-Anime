eb54f669866666a01d8d3526fbd439e335d41b57
Aluno <aluno@aluno.com>
2019-04-17 09:59:12 -0300

    Alterações

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
-<template>
-  <v-app id="inspire">
+<template >
+  <v-app style="
+    background: black;
+  ">
     <v-navigation-drawer
       v-model="drawerRight"
       fixed
@@ -7,85 +9,50 @@
       clipped
       app
     >
-      <v-list dense>
-        <v-list-tile @click.stop="right = !right">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+     
     </v-navigation-drawer>
     <v-toolbar
-      color="dark"
-      dark
+      black
       fixed
       app
       clipped-right
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Crazy Animes</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-side-icon @click.stop="drawerRight = !drawerRight"></v-toolbar-side-icon>
+    <v-spacer>
+
+      
+
+
+
+    </v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>  
+
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+
     </v-toolbar>
+
+    
     <v-navigation-drawer
       v-model="drawer"
       fixed
       app
     >
-      <v-list dense>
-        <v-list-tile @click.stop="left = !left">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
     </v-navigation-drawer>
-    <v-navigation-drawer
-      v-model="left"
-      temporary
-      fixed
-    ></v-navigation-drawer>
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-flex shrink>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>code</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn icon large href="https://codepen.io/johnjleider/pen/KQrPKJ" target="_blank" v-on="on">
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="right"
-      right
-      temporary
-      fixed
-    ></v-navigation-drawer>
-    <v-footer color="blue-grey" class="white--text" app>
-      <span>Vuetify</span>
-      <v-spacer></v-spacer>
-      <span>&copy; 2017</span>
-    </v-footer>
+      
   </v-app>
 </template>
 
@@ -102,3 +69,5 @@
     }
   }
 </script>
+
+
