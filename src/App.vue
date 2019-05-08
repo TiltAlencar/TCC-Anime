103f2fa906d9f6d22282d777a22e651963c5036f
Aluno <aluno@aluno.com>
2019-05-08 09:41:27 -0300

    Carrossel

--- src/App.vue
+++ src/App.vue
@@ -73,136 +73,154 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a500ad', end
       <v-btn flat>Mangás</v-btn>
 
 <!-- botão login -->
-      <template>
-        <div class="text-xs-center">
-          <v-dialog
-            v-model="dialog"
-            width="500"
-          >
-            <template v-slot:activator="{ on }">
-              <v-btn
-              
-                dark
-                v-on="on"
+    <template>
+      <div class="text-xs-center">
+        <v-dialog
+          v-model="dialog"
+          width="500"
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+            
+              dark
+              v-on="on"
+            >
+              Ainda não tem uma conta?
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title
+              class="headline grey lighten-2"
+              primary-title
+            >
+              REGISTRAR CONTA!
+            </v-card-title>
+
+            <v-card-text>
+              <template>
+              <v-card
+                class="mx-auto"
+                max-width="500"
               >
-                Ainda não tem uma conta?
-              </v-btn>
+                <v-card-title class="title font-weight-regular justify-space-between">
+                  <span>{{ currentTitle }}</span>
+                  <v-avatar
+                    color="primary lighten-2"
+                    class="subheading white--text"
+                    size="24"
+                    v-text="step"
+                  ></v-avatar>
+                </v-card-title>
+
+                <v-window v-model="step">
+                  <v-window-item :value="1">
+                    <v-card-text>
+                      <v-text-field
+                        label="Email"
+
+                      ></v-text-field>
+                      <span class="caption grey--text text--darken-1">
+                        Insira seu email acima!!
+                      </span>
+                    </v-card-text>
+                  </v-window-item>
+
+                  <v-window-item :value="2">
+                    <v-card-text>
+                      <v-text-field
+                        label="Senha"
+                        type="password"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Confirme sua senha"
+                        type="password"
+                      ></v-text-field>
+                      <span class="caption grey--text text--darken-1">
+                        Insira sua senha acima!!
+                      </span>
+                    </v-card-text>
+                  </v-window-item>
+
+                  <v-window-item :value="3">
+                    <div class="pa-3 text-xs-center">
+                      <v-img
+                        class="mb-3"
+                        contain
+                        height="128"
+                        src="IMAGEM AQUI"
+                      ></v-img>
+                      <h3 class="title font-weight-light mb-2">Bem Vindo ao Crazy Animes!</h3>
+                      <span class="caption grey--text">Obrigado por criar sua conta! sua alma agora é nossa ;)</span>
+                    </div>
+                  </v-window-item>
+                </v-window>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-btn
+                    :disabled="step === 1"
+                    flat
+                    @click="step--"
+                  >
+                    Voltar
+                  </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    :disabled="step === 3"
+                    color="primary"
+                    depressed
+                    @click="step++"
+                  >
+                    Próximo
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
             </template>
-
-            <v-card>
-              <v-card-title
-                class="headline grey lighten-2"
-                primary-title
-              >
-                REGISTRAR CONTA!
-              </v-card-title>
-
-              <v-card-text>
-                <template>
-                <v-card
-                  class="mx-auto"
-                  max-width="500"
-                >
-                  <v-card-title class="title font-weight-regular justify-space-between">
-                    <span>{{ currentTitle }}</span>
-                    <v-avatar
-                      color="primary lighten-2"
-                      class="subheading white--text"
-                      size="24"
-                      v-text="step"
-                    ></v-avatar>
-                  </v-card-title>
-
-                  <v-window v-model="step">
-                    <v-window-item :value="1">
-                      <v-card-text>
-                        <v-text-field
-                          label="Email"
-
-                        ></v-text-field>
-                        <span class="caption grey--text text--darken-1">
-                         Insira seu email acima!!
-                        </span>
-                      </v-card-text>
-                    </v-window-item>
-
-                    <v-window-item :value="2">
-                      <v-card-text>
-                        <v-text-field
-                          label="Senha"
-                          type="password"
-                        ></v-text-field>
-                        <v-text-field
-                          label="Confirme sua senha"
-                          type="password"
-                        ></v-text-field>
-                        <span class="caption grey--text text--darken-1">
-                         Insira sua senha acima!!
-                        </span>
-                      </v-card-text>
-                    </v-window-item>
-
-                    <v-window-item :value="3">
-                      <div class="pa-3 text-xs-center">
-                        <v-img
-                          class="mb-3"
-                          contain
-                          height="128"
-                          src="IMAGEM AQUI"
-                        ></v-img>
-                        <h3 class="title font-weight-light mb-2">Bem Vindo ao Crazy Animes!</h3>
-                        <span class="caption grey--text">Obrigado por criar sua conta! sua alma agora é nossa ;)</span>
-                      </div>
-                    </v-window-item>
-                  </v-window>
-
-                  <v-divider></v-divider>
-
-                  <v-card-actions>
-                    <v-btn
-                      :disabled="step === 1"
-                      flat
-                      @click="step--"
-                    >
-                      Voltar
-                    </v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      :disabled="step === 3"
-                      color="primary"
-                      depressed
-                      @click="step++"
-                    >
-                      Próximo
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </template>
-              </v-card-text>
-              <v-divider></v-divider>
-            </v-card>
-          </v-dialog>
-        </div>
-      </template>
+            </v-card-text>
+            <v-divider></v-divider>
+          </v-card>
+        </v-dialog>
+      </div>
+    </template>
 
 <!-- fim botão login -->
       
       </v-toolbar>
 
 <!-- Carrossel -->
-    <template>
-  <v-carousel hide-controls>
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
-    </template>
-<!-- 
+  <div id="carrossel">
+  <template>
+    <v-carousel hide-controls>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+  </template>
+  </div>
+<!-- Fim carrossel -->
     
   </v-app>
 </template>
+
+
+<style>
+
+#carrossel{
+  width:95%;
+  display:flex;
+  justify-content:10px;
+  height:16%;
+  margin-top:8%;
+  margin-left:2.5%;
+  position:absolute;
+}
+
+</style>
+
 
 <script>
   export default {
@@ -225,7 +243,6 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a500ad', end
     }
   }
 </script>
-
 
 <script>
   export default {
@@ -271,3 +288,5 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a500ad', end
 </script>
 
 <!-- Fim Carrossel -->
+
+
